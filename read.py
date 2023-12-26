72d28aab89dd58626eea0f3d3497ea2657922a54
asalkhezli <154859795+asalkhezli@users.noreply.github.com>
2023-12-26 09:50:21 +0330

    Init

---
+++ read.py
@@ -0,0 +1,10 @@
+#import pandas as pd
+#df = pd.read_excel("C:\Users\3979910229\Desktop\1\khezli\11.xlsx") 
+#print  (df)
+
+import pandas as pd
+
+from openpyxl import load_workbook
+data = pd.read_excel (r'C:\Users\3979910229\Desktop\1\khezli\11.xlsx')
+
+print (data)
